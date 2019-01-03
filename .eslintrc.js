cca1aa933e609015973ebebda6909e8ca9b39989
Samu Joseph <samujoseph@outlook.com>
2019-01-03 19:27:32 +0530

    added config files for basic setup

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "jest": true,
+        "node": true,
+        "commonjs": true
+    },
+    "rules": {
+      "strict": 0,
+      "semi": 2,
+      "no-undef": 2,
+      "no-unused-vars": 1
+    },
+    "parserOptions": {
+        "sourceType": "module",
+        "ecmaVersion": 2018
+    },
+    "extends": "eslint:recommended"
+};
